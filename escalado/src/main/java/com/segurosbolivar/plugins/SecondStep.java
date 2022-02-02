dde175f600d4816e30eddbffa6ce07ab74c42dbf
Sebastian Bejarano <slbejaranom@gmail.com>
2022-02-02 10:57:18 -0500

    Trabajando en el paso 2 con acción POST

---
+++ escalado/src/main/java/com/segurosbolivar/plugins/SecondStep.java
@@ -0,0 +1,86 @@
+package com.segurosbolivar.plugins;
+
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import com.atlassian.sal.api.auth.LoginUriProvider;
+import com.atlassian.sal.api.user.UserManager;
+import com.atlassian.sal.api.user.UserProfile;
+import com.atlassian.templaterenderer.TemplateRenderer;
+import com.atlassian.webresource.api.assembler.PageBuilderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SecondStep extends HttpServlet {
+
+    private static final Logger log = LoggerFactory.getLogger(PluginServlet.class);
+
+    @ComponentImport
+    private final UserManager userManager;
+
+    @ComponentImport
+    private final LoginUriProvider loginUriProvider;
+
+    @ComponentImport
+    private final TemplateRenderer templateRenderer;
+
+    @ComponentImport
+    private final PageBuilderService pageBuilderService;
+
+    @Inject
+    public SecondStep(UserManager userManager, LoginUriProvider loginUriProvider, TemplateRenderer templateRenderer, PageBuilderService pageBuilderService){
+        this.userManager = userManager;
+        this.loginUriProvider = loginUriProvider;
+        this.templateRenderer = templateRenderer;
+        this.pageBuilderService = pageBuilderService;
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+
+    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        //Enviamos el recurso de CSS
+        pageBuilderService.assembler().resources().requireWebResource("com.segurosbolivar.plugins.escalado:escalado-resources");
+        //Hacemos el mapa entre parámetros y objetos
+        Map<String,Object> params = new HashMap<String,Object>();
+        //Se obtiene información sobre el usuario que realiza la petición
+        UserProfile user = userManager.getRemoteUser(req);
+
+
+        String userName = user.getUsername();
+
+        if(userName == null){
+            redirectToLogin(req,res);
+            return;
+        }
+        res.setContentType("text/html; charset=utf-8");
+        templateRenderer.render("templates/inicio.vm", params,res.getWriter());
+
+    }
+
+    private void redirectToLogin(HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+        response.sendRedirect(loginUriProvider.getLoginUri(getUri(request)).toASCIIString());
+    }
+    private URI getUri(HttpServletRequest request)
+    {
+        StringBuffer builder = request.getRequestURL();
+        if (request.getQueryString() != null)
+        {
+            builder.append("?");
+            builder.append(request.getQueryString());
+        }
+        return URI.create(builder.toString());
+    }
+}
--- escalado/src/main/resources/js/escalado.js
+++ escalado/src/main/resources/js/escalado.js
@@ -14,6 +14,18 @@ const window_callback = () =>{
             "transform":"translate(-23%, -50%)"
         }
     );
+    $(".groupInput").css(
+        {
+            "border":"1px solid black",
+            "width":"20%",
+            "margin-left":"auto",
+            "margin-right":"auto"
+        }
+    );
+    $("#closeBtn").click(() => {
+        $("#mi-item-link-dialog").remove();
+        $(".aui-blanket").remove();
+    });
 }
 const observer = new MutationObserver(function(mutations_list) {
    	mutations_list.forEach(function(mutation) {
@@ -21,7 +33,7 @@ const observer = new MutationObserver(function(mutations_list) {
    			if(added_node.id == 'mi-item-link-dialog') {
    				console.log('mi-item-link-dialog has been added');
    				$("#mi-item-link-dialog").ready(()=>{
-                    setTimeout(window_callback,100);
+                    setTimeout(window_callback,50);
    				});
    				observer.disconnect();
    			}
